fa8576a38cd55e4c574a42f064e3699a2eefe5ca
OrlandoMts <dani_monts19@hotmail.com>
2021-11-08 20:26:00 -0600

    instale y configure axios. Ymanejo del estado del toggle

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,20 @@
 import React from 'react';
 import '../styles/componentsStyle/Header.scss';
+import { Menu } from '@components/Menu';
+
 import icon_menu from '@icons/icon_menu.svg';
 import logo_yard_sale from '@logos/logo_yard_sale.svg';
 import icon_shopping from '@icons/icon_shopping_cart.svg';
 
 const Header = () => {
+
+    const [ toggle, setToggle ] = React.useState(false);
+
+    const handleToggle = () => {
+        console.log('hola');
+        setToggle(!toggle);
+    }
+
     return (
         <nav>
             <img src={icon_menu} alt="menu" className="menu" />
@@ -33,13 +43,18 @@ const Header = () => {
             </div>
             <div className="navbar-right">
                 <ul>
-                    <li className="navbar-email">platzi@example.com</li>
+                    <li className="navbar-email" onClick={handleToggle}>
+                        orlandomontes97@outlook.com
+                    </li>
                     <li className="navbar-shopping-cart">
                         <img src={icon_shopping} alt="shopping cart" />
                         <div>2</div>
                     </li>
                 </ul>
             </div>
+            {/* Si toggle es false no se va a mostrar */}
+            {toggle && <Menu />}
+            
     </nav>
     )
 }
